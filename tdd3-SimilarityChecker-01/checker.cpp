76716f0d21ba4381d9ce6acadf23c1b257b6cdf0
Jinwoo <jnstar@gmail.com>
2023-06-02 11:58:26 +0900

    [refactoring] update getScoreWithLengh function
    Signed-off-by: Jinwoo <jnstar@gmail.com>

--- tdd3-SimilarityChecker-01/checker.cpp
+++ tdd3-SimilarityChecker-01/checker.cpp
@@ -4,10 +4,16 @@
 using namespace std;
 
 const int MAX_SCORE_LENGTH = 60;
+const int SCORE_DOUBLE_LENGTH = 0;
 
 class SimilarityChecker
 {
 public:
+	bool isDoubleLength(int len1, int len2)
+	{
+		return len1 >= len2 * 2 || len2 >= len1 * 2;
+	}
+
 	int getScoreWithLength(const string& input1, const std::string& input2)
 	{
 		int len1 = input1.length();
@@ -18,6 +24,11 @@ public:
 			return MAX_SCORE_LENGTH;
 		}
 
-		return 0;
+		if (isDoubleLength(len1, len2))
+		{
+			return SCORE_DOUBLE_LENGTH;
+		}
+
+		return -1;
 	}
 };
