46b0926a7ebe10941bda736c371a8b15aa3a72de
Jinwoo <jnstar@gmail.com>
2023-06-02 11:41:49 +0900

    [feature] add function getScoreWithLength
    Signed-off-by: Jinwoo <jnstar@gmail.com>

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,10 @@
 #include "pch.h"
 #include "../tdd3-SimilarityChecker-01/checker.cpp"
 
-TEST(SimilarityChecker, TestName) {
-	
+TEST(SimilarityChecker, compareStringLength01) {
+	string input1 = "ASD";
+	string input2 = "DSA";
+	int result = SimilarityChecker().getScoreWithLength(input1, input2);
+	EXPECT_EQ(100, result);
 }
 
--- tdd3-SimilarityChecker-01/checker.cpp
+++ tdd3-SimilarityChecker-01/checker.cpp
@@ -5,5 +5,9 @@ using namespace std;
 
 class SimilarityChecker
 {
-
+public:
+	int getScoreWithLength(const string& string, const std::string& input2)
+	{
+		return 100;
+	}
 };
